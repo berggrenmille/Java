e8c601ffdd0e28613e0fe9469df87d865457a70d
iceklue <iceklue@gmail.com>
2017-08-21 07:49:50 +0200

    Added new template class

---
+++ Java Workspace/Programmering 1/src/edu/mille/java/uppgifter/U7_5.java
@@ -0,0 +1,11 @@
+package edu.mille.java.uppgifter;
+
+import java.util.Scanner;
+
+//2017-08-21
+public class U7_5 {
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+    }
+}
